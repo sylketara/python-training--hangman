12a95ac7a025fb026606ee26f20756959d457bbf
sylke <sylke.goldsmith@digital.hmrc.gov.uk>
2017-12-06 11:37:02 +0000

    completed

--- hangman2.py
+++ hangman2.py
@@ -4,37 +4,48 @@ import random
 name = raw_input("can I have your name please?")
 print"hello" +name, "let's play hangman!"
 print ""
-#print"Take a guess!"
 
 # function to define and set a random word
 def choose_word():
     wordlist = ["christmas", "elf", "santa", "rudolph"]
     chosenword = random.choice(wordlist)
     print("Random word has been chosen")
-    handle_guess(chosenword)
+    handle_guess(chosenword, letters_used)
 
+letters_used = ""
 #define a guess
-def handle_guess(chosenword):
-    print(chosenword)
+def handle_guess(chosenword, letters_used):
+    #print(chosenword)
 #set number of guesses on while loop
     number_of_guesses = 0
-    while number_of_guesses < 10:
+    while number_of_guesses < 15:
         number_of_guesses = number_of_guesses +1
         guess = raw_input("have a guess:")
+        #making users guess always lower case
+        guess = guess.lower()
         print(letters_used)
         if guess in letters_used:
             print "You have already used this letter. Try again"
         else:
+            #add chosen letter to string of used letters
+            letters_used = letters_used + guess
+            wrong = 0
+            for letter in chosenword:
+                if letter in letters_used:
+                    print letter,
+                else:
+                    print "_",
+                    wrong = wrong + 1
+
             if guess in chosenword:
-                print "You've guessed correctly! Guess some more"
+                print "You've guessed correctly!"
+                if wrong == 0:
+                    print "You've won!"
+                    #ending while loop
+                    break
             else:
                 print "nope! try again"
-            if number_of_guesses > 10:
-                print "You've lost!"
 
 
-
-
-letters_used = ""
 chosenword = ""
 choose_word()
